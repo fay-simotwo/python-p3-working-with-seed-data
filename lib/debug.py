c3fdf90d534b1b54a2db934be0a975752ce2d5e7
Moringa School <chepfaith18@gmail.com>
2023-08-31 15:45:12 +0300

    Completed Assignment

--- lib/debug.py
+++ lib/debug.py
@@ -5,15 +5,41 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game
-
-fake = Faker()
+from models import Base, Game  # Import the Base and Game classes
 
 if __name__ == '__main__':
-    
+    fake = Faker()
+
+    # Connect to the database
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Clear old data from the games table
+    session.query(Game).delete()
+    session.commit()
 
-    import ipdb; ipdb.set_trace()
+    # Add new data using Faker
+    print("Seeding games...")
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        for _ in range(50)
+    ]
+
+    session.add_all(games)
+    session.commit()
+
+    # Count the number of records
+    game_count = session.query(Game).count()
+    print(f"Total games seeded: {game_count}")
+
+    # Print the first and last records
+    first_game = session.query(Game).first()
+    last_game = session.query(Game).order_by(Game.id.desc()).first()
+    print(f"First game: {first_game}")
+    print(f"Last game: {last_game}")
--- lib/seed.py
+++ lib/seed.py
@@ -5,12 +5,24 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game
+from models import Game  # Import the Game class from your models
 
 fake = Faker()
 
 if __name__ == '__main__':
-    
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # Clear old data
+    session.query(Game).delete()
+    session.commit()
+
+    # Create new data
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+    ccs = Game(title="Candy Crush Saga", platform="Mobile", genre="Puzzle", price=0)
+
+    session.add_all([botw, ffvii, mk8, ccs])
+    session.commit()
